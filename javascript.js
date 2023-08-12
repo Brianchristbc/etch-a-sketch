870a490dcbab637711c570a1f13c0906fed7e485
Brianchristbc <138142173+Brianchristbc@users.noreply.github.com>
2023-08-13 00:25:55 +0800

    Create a UI for the webpage

--- javascript.js
+++ javascript.js
@@ -1,60 +1,138 @@
-for (let i=0; i<256; i++) {
-  const sketch = document.createElement('div');
-  sketch.classList.add('sketch');
-  sketch.style.width = "10px";
-  sketch.style.height = "10px";
-  sketch.style.background = "aliceblue"
-  sketch.style.margin = "0";
-  const container = document.querySelector('.container');
-  container.appendChild(sketch);
+const container = document.querySelector('.container')
+let squares;
+
+const squareInput = document.getElementById('square-input');
+const squareInputButton = document.querySelector('.square-input-button');
+const updated = document.querySelector('.updated')
+const canvasPicker = document.getElementById('canvas-color-picker')
+canvasPicker.value = '#F5F5F5'
+const brushPicker = document.getElementById('brush-color-picker');
+brushPicker.value = '#FFC0CB'
+
+window.addEventListener('load', () => {
+  squares = 100;
+  let lengthOfSquares = 480/squares;
+  let numberOfSquares = squares**2; 
+  createCanvas(numberOfSquares, lengthOfSquares, canvasPicker.value || '#F5F5F5');
+
+  const square = document.querySelectorAll('.square');
+  
+  canvasPicker.addEventListener('change', function pickColor() {
+    square.forEach((element) => {
+      element.style.background = canvasPicker.value
+    })
+  })
+})
+
+squareInputButton.addEventListener('click', () => {
+  squares = (squareInput.value)*1;
+  squareInput.value = "";
+  let lengthOfSquares = 480/squares;
+  let numberOfSquares = squares**2; 
+  createCanvas(numberOfSquares, lengthOfSquares, canvasPicker.value || '#F5F5F5');
+  updated.textContent = "updated!"
+
+  const square = document.querySelectorAll('.square');
+  
+  canvasPicker.addEventListener('change', function pickColor() {
+    square.forEach((element) => {
+      element.style.background = canvasPicker.value
+    })
+  })
+})
+
+
+squareInput.addEventListener('keydown', (e) => {
+  if (e.key === "Enter") {
+    squares = (squareInput.value)*1;
+    squareInput.value = "";
+    let lengthOfSquares = 480/squares;
+    let numberOfSquares = squares**2; 
+    createCanvas(numberOfSquares, lengthOfSquares, canvasPicker.value || '#F5F5F5');
+    updated.textContent = "updated!"
+  
+    const square = document.querySelectorAll('.square');
+    
+    canvasPicker.addEventListener('change', function pickColor() {
+      square.forEach((element) => {
+        element.style.background = canvasPicker.value
+      })
+    })
+  }
+})
+
+function createCanvas(numberOfSquares, lengthOfSquares, color) {
+  container.innerHTML = "";
+  for(let i=0; i<numberOfSquares; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square')
+    square.style.height = `${lengthOfSquares}px`;
+    square.style.width = `${lengthOfSquares}px`;
+    square.style.background = canvasPicker.value;
+    container.appendChild(square);
+  }
 }
 
-const sketch = document.querySelectorAll('.sketch')
+let brushColor = 'Pink';
 
-sketch.forEach((sketch) => {
-  sketch.addEventListener('mouseover', () => {
-    sketch.style.background = randomColorGenerator();
-  })
+function pickBrushColor() {
+  brushColor = brushPicker.value;
+  return brushColor;
+}
+
+brushPicker.addEventListener('change', () => {
+  pickBrushColor();
 })
 
-const reset = document.querySelector('.reset')
-
-reset.addEventListener('click', () => {
-  sketch.forEach((sketch) => {
-    sketch.style.background = (colorOfChoice || "#ff0000")
-  })
+container.addEventListener('mouseover', (e) => {
+  const square = e.target;
+  square.style.background = brushColor
 })
-
-colorPicker = document.querySelector('.color-picker');
-// addeventlistener can be fired with input and change
-// event.target.value
-
-colorPicker.addEventListener('input', (e) => {
-  colorOfChoice = e.target.value;
-})
-
-colorPicker.addEventListener('change', (e) => {
-  colorOfChoice = e.target.value;
-})
-// when can i use this instead of e
 
 function randomColorGenerator() {
-  const red = Math.floor((Math.random())*255);
-  const green = Math.floor((Math.random())*255);
-  const blue = Math.floor((Math.random())*255);
-  return `rgb(${red}, ${green}, ${blue})`
+  const red = (Math.floor(Math.random()*255));
+  const green = (Math.floor(Math.random()*255));
+  const blue = (Math.floor(Math.random()*255));
+  const rgb = `rgb(${red}, ${green}, ${blue})`;
+  return rgb;
 }
 
-const rainbowText = document.querySelector('.rainbow-text');
 
-rainbowText.style.color = randomColorGenerator()
+const rainbowButton = document.querySelector('.rainbow-button');
+
+rainbowButton.addEventListener('click', () => {
+  brushToggle()})
+  
+
+  function brushToggle() {
+    const displayToggle = document.querySelector('.toggle-row');
+    if (!(rainbowButton.classList.contains('activated'))) {
+      rainbowButton.classList.toggle('activated')
+      displayToggle.textContent = 'toggled';
+      container.removeEventListener('mouseover', (e) => {
+        const square = e.target;
+        square.style.background = brushColor
+      })
+      container.addEventListener('mouseover', (e) => {
+        const square = e.target;
+        square.style.background = randomColorGenerator()
+      })
+      }
+
+      else if (rainbowButton.classList.contains('activated')) {
+        rainbowButton.classList.toggle('activated')
+        displayToggle.textContent = '';
+        container.removeEventListener('mouseover', (e) => {
+          const square = e.target;
+          square.style.background = randomColorGenerator()
+        })
+        container.addEventListener('mouseover', (e) => {
+          const square = e.target;
+          square.style.background = brushColor
+        })
+      }
+  }
 
 
 
 
-
-
-
-// const p = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
-
-// console.log(p.replace('dog', 'monkey'));
